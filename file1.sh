66eb9ba0fe171f97131752e887b62f2b94899294
root <root@ip-172-31-0-99.ec2.internal>
2021-05-18 09:48:42 +0000

    Edited ping count to 6 from 5.

---
+++ file1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash/bash
+echo "I'm create a shell script which can take input of a website and ping if for 5 times"
+read -p "Please enter the website(Eg: www.google.com)"
+ping  -c 5 $website
